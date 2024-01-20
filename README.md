7efa14a4246e9fd6b83fb9183a3bcc75020cf168
Greg Pasquariello <greg@pasq.net>
2024-01-19 22:53:27 -0800

    Add source files.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# newver
+To install this locally, check out the project, cd to the folder, then
+run 
+
+```npm install -g .```
+
+After completion, you can run `node version` in any folder with a `CURRENT_VERSION` file.
---
+++ newver.js
@@ -0,0 +1,23 @@
+
+const fs = require('node:fs');
+try {
+    const v = fs.readFileSync('CURRENT_VERSION', 'utf8');
+    const vals = v.split(".")
+
+    if (vals.length !== 3) {
+        console.log("Version not structured correctly")
+    }
+
+    const major = vals[0]
+    const minor = vals[1]
+    const patch = vals[2]
+
+    console.log("Current version: " + major + "." + minor + "." + patch)
+    console.log("New version: " + major + "." + minor + "." + (Number(patch) + 1))
+
+    fs.writeFileSync("CURRENT_VERSION", major + "." + minor + "." + (Number(patch) + 1))
+
+} catch (err) {
+    console.error(err);
+}
+
